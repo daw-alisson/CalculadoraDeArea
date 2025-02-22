a23929da2457cb404bb35b2dd52d2a1df6f1ad5c
daw.alisson <daw.alisson@gmail.com>
2025-02-22 14:59:06 +0100

    Cración de método trianguloaArea

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -3,9 +3,21 @@ package org.example;
 public class AreaCalculator {
     public static void main(String[] args) {
         System.out.println("Calculadora de área para diferentes figuras.");
+
+        double base = 4;
+        double altura = 2;
+        // llamada al metodo triangleArea y guardo el valor devuelto por el método.
+        double resultadoAreaTriangulo = triangleArea(base, altura);
+        System.out.println("Resultado del area del triangulo: " + resultadoAreaTriangulo);
+
     }
     public static double circleArea(double radius) {
         return Math.PI * radius * radius;
     }
-// Añadir métodos para calcular el área de un rectángulo y un triángulo
+
+    // AlumnoB: Alisson Espin
+    public static double triangleArea(double base, double altura) {
+        double operacion = (base * altura) / 2;
+        return operacion;
+    }
 }
